901e414e44380980fca34b1f2df2a3011cbbc90b
yohanespanjaitan04 <yohanes.panjaitan04@gmail.com>
2024-12-01 14:21:58 +0700

    rapihin header dan nambahin sidenav

--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -23,20 +23,22 @@
         .header {
             background-color: #658345;
             color: black;
-            padding: 30px;
-            text-align: left;
+            padding: 30px 0;
+            width: 100%;
+            margin: 0;
+        }   
+        .header-content {
             display: flex;
             align-items: center;
-            justify-content: space-between;
-        }   
+            padding: 0 40px;
+        }  
         .header h1 {
             margin: 0;
             font-size: 30px;
-            margin-left: 120px; 
+            margin-left: 20px; 
         }
         .header img {
             height: 60px;
-            margin-right: 20px;
         }
         .content-wrapper {
             display: flex;
@@ -161,12 +163,12 @@
 </head>
 <body>
 
-<div class="header">
-    <div>
-        <img src="{{ asset('backend\img\logoSate-removebg-preview.png') }}" alt="">
-        <h1>SATE <br><small>Sistem Akademik Terpadu Efisien</small></h1>
-    </div>
-</div>
+<header class="header">
+        <div class="header-content">
+            <img src="{{ asset('backend\img\logoSate-removebg-preview.png') }}" alt="SATE Logo">
+            <h1>SATE <br><small>Sistem Akademik Terpadu Efisien</small></h1>
+        </div>
+</header>
 
 <div class="content-wrapper">
     <div class="sidebar">
