5a1e8f5933c6b8814f69c971b461f954f565241c
faiqilmupadi <muhammadfaiqas88@gmail.com>
2024-12-08 10:49:33 +0700

    faiqbarulg

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -9,14 +9,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        body {
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f0f0f0;
-        }
+    body {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-image: url('{{ asset("backend/img/undip.jpg") }}');
+        background-size: cover; /* Agar gambar menutupi seluruh area */
+        background-repeat: no-repeat; /* Agar gambar tidak diulang */
+        background-position: center; /* Agar gambar berada di tengah */
+    }
+
 
         .login-container {
             background-color: white;
